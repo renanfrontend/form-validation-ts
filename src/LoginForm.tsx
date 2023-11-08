ad7b4d21fd41b4af8d72a87b0bcc9186008891b0
Renan Augusto <renan.santos_ext@olos.com.br>
2023-11-08 13:37:47 -0300

    Validação dos caracteres conforme digitamos

--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -1,30 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { validatePassword } from './validation';
 
 const LoginForm: React.FC = () => {
   const [password, setPassword] = useState('');
   const [errors, setErrors] = useState<string[]>([]);
+  const [success, setSuccess] = useState<string[]>([]);
+
+  useEffect(() => {
+    const validationResults = validatePassword(password);
+    setErrors(validationResults.errors);
+    setSuccess(validationResults.success);
+  }, [password]);
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
 
-    const validationErrors = validatePassword(password);
-    if (validationErrors.length > 0) {
-      setErrors(validationErrors);
-    } else {
+    if (errors.length === 0) {
       // Se a validação for bem-sucedida, prossiga com o login...
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-    
       <label>
         Senha:
         <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
       </label>
       <button type="submit">Login</button>
-      {errors.map((error, index) => <p key={index}>{error}</p>)}
+      {errors.map((error, index) => (
+        <p key={index}>
+          <span role="img" aria-label="error">❌</span>
+          {error}
+        </p>
+      ))}
+      {success.map((message, index) => (
+        <p key={index}>
+          <span role="img" aria-label="check">✅</span>
+          {message}
+        </p>
+      ))}
     </form>
   );
 };
--- src/validation.ts
+++ src/validation.ts
@@ -1,25 +1,36 @@
-export function validatePassword(password: string): string[] {
-    const errors = [];
-  
-    if (password.length < 8) {
-      errors.push('A senha deve ter pelo menos 8 caracteres');
-    }
-  
-    if (!/\d/.test(password)) {
-      errors.push('A senha deve conter pelo menos 1 número');
-    }
-  
-    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
-      errors.push('A senha deve conter pelo menos 1 caractere especial');
-    }
-  
-    if (!/[A-Z]/.test(password)) {
-      errors.push('A senha deve conter pelo menos 1 letra maiúscula');
-    }
-  
-    if (!/[a-z]/.test(password)) {
-      errors.push('A senha deve conter pelo menos 1 letra minúscula');
-    }
-  
-    return errors;
+export function validatePassword(password: string): {errors: string[], success: string[]} {
+  const errors = [];
+  const success = [];
+
+  if (password.length < 8) {
+    errors.push('A senha deve ter pelo menos 8 caracteres');
+  } else {
+    success.push('A senha tem pelo menos 8 caracteres');
   }
+
+  if (!/\d/.test(password)) {
+    errors.push('A senha deve conter pelo menos 1 número');
+  } else {
+    success.push('A senha contém pelo menos 1 número');
+  }
+
+  if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+    errors.push('A senha deve conter pelo menos 1 caractere especial');
+  } else {
+    success.push('A senha contém pelo menos 1 caractere especial');
+  }
+
+  if (!/[A-Z]/.test(password)) {
+    errors.push('A senha deve conter pelo menos 1 letra maiúscula');
+  } else {
+    success.push('A senha contém pelo menos 1 letra maiúscula');
+  }
+
+  if (!/[a-z]/.test(password)) {
+    errors.push('A senha deve conter pelo menos 1 letra minúscula');
+  } else {
+    success.push('A senha contém pelo menos 1 letra minúscula');
+  }
+
+  return {errors, success};
+}
